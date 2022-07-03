a89e8c0f0b6eebb6788659451abe2abbd1610009
Luis Alfonso A. Leh <luisleh@Luiss-MacBook-Pro.local>
2022-07-03 16:39:51 +0800

    added seed data

--- server/db/seeds/1-recyclables.js
+++ server/db/seeds/1-recyclables.js
@@ -1,2 +1,63 @@
-// @param { import("knex").Knex } knex
-// @returns {} Promise<void> }
+exports.seed = function (knex) {
+  return knex('recyclables').insert([
+    {
+      id: 1,
+      recyclable_item: 'Soft drink bottles',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 2,
+      recyclable_item: 'Milk bottles',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 3,
+      recyclable_item: 'Ice cream containers',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 4,
+      recyclable_item: 'Family-sized yoghurt containers',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 5,
+      recyclable_item: 'Metal tins (not squashed)',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 6,
+      recyclable_item: 'Aerosol cans (not squashed)',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 7,
+      recyclable_item: 'Glass bottles',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 8,
+      recyclable_item: 'Glass jars',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 9,
+      recyclable_item: 'Paper',
+      recyclable_item_image: '',
+    },
+
+    {
+      id: 10,
+      recyclable_item: 'Flattened cardboard boxes',
+      recyclable_item_image: '',
+    },
+  ])
+}
--- server/db/seeds/3-general.js
+++ server/db/seeds/3-general.js
@@ -1,2 +1,57 @@
-// @param { import("knex").Knex } knex
-// @returns { Promise<void> }
+exports.seed = function (knex) {
+  return knex('general').insert([
+    {
+      id: 1,
+      general_item: 'Takeaway drink cups',
+      general_item_image: '',
+    },
+
+    {
+      id: 2,
+      general_item: 'Takeaway containers',
+      general_item_image: '',
+    },
+
+    {
+      id: 3,
+      general_item: 'Containers over 3 litres',
+      general_item_image: '',
+    },
+
+    {
+      id: 4,
+      general_item: 'All lids',
+      general_item_image: '',
+    },
+
+    {
+      id: 5,
+      general_item: 'Fabrics, textile and clothing',
+      general_item_image: '',
+    },
+
+    {
+      id: 6,
+      general_item: 'Soft plastics',
+      general_item_image: '',
+    },
+
+    {
+      id: 7,
+      general_item: 'Liquid cartons',
+      general_item_image: '',
+    },
+
+    {
+      id: 8,
+      general_item: 'Compostable and bio-degradable packaging',
+      general_item_image: '',
+    },
+
+    {
+      id: 9,
+      general_item: 'Ash, timber and sawdust',
+      general_item_image: '',
+    },
+  ])
+}
